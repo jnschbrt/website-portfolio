3f8dfb47ba6ba63ca489220d7f63cb913755485d
Jonas Schubert <schubert.jonas@web.de>
2020-04-08 21:41:53 +0200

    link fixed

--- src/components/About.vue
+++ src/components/About.vue
@@ -90,7 +90,7 @@ export default {
         },
         {
           icon: 'mdi-linkedin',
-          link: 'www.linkedin.com/in/schubert-jonas',
+          link: 'https://www.linkedin.com/in/schubert-jonas',
           tooltip: 'LinkedIn'
         }
       ]
