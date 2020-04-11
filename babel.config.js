7a938d4e54a69778c76612c414bcdb2a2c2b5678
Jonas Schubert <schubert.jonas@web.de>
2020-04-11 11:08:45 +0200

    wrong config, upsi

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return
-    return {
-      plugins: [
-        new PrerenderSPAPlugin(
-          // Absolute path to compiled SPA
-          path.resolve(__dirname, 'dist'),
-          // List of routes to prerender
-          ['/'],
-          {
-            // options
-          }
-        )
-      ]
-    }
-  }
+  presets: ['@vue/cli-plugin-babel/preset']
 }
